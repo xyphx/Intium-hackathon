a987ae3a04b692758c3a098d070241904801e7c9
S SIDHARTH <sidharthpunalur@gmail.com>
2026-08-20 15:39:37 +0530

    fix: allow all CORS origins to support access from local Wi-Fi IP

--- server/app/main.py
+++ server/app/main.py
@@ -16,16 +16,9 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-# Allow React app development server
-origins = [
-    "http://localhost:5173", # Vite default
-    "http://127.0.0.1:5173",
-    "http://localhost:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
